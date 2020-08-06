9fdbd33b3f8329f1a3b6d1abb543ade25c9f1962
Jeferson <36243087+devjef@users.noreply.github.com>
2020-08-06 11:43:58 -0300

    mudança link heroku

--- src/config/index.js
+++ src/config/index.js
@@ -1,7 +1,7 @@
 /* eslint-disable linebreak-style */
 const URL_BACKEND_TOP = window.location.hostname.includes('localhost')
   ? 'http://localhost:8080'
-  : 'https://devsoutinhoflix.herokuapp.com';
+  : 'https://jefflixdb.herokuapp.com';
 
 export default {
   URL_BACKEND_TOP,
--- src/pages/cadastro/Categoria/index.js
+++ src/pages/cadastro/Categoria/index.js
@@ -20,7 +20,7 @@ function CadastroCategoria() {
   useEffect(() => {
     const URL_TOP = window.location.hostname.includes('localhost')
       ? 'http://localhost:8080/categorias'
-      : 'https://devsoutinhoflix.herokuapp.com/categorias';
+      : 'https://jefflixdb.herokuapp.com';
     fetch(URL_TOP)
       .then(async (respostaDoServidor) => {
         const resposta = await respostaDoServidor.json();
